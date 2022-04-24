00091c8cce9dd3a5b39c1e1eb619da7887ee4de7
silexoider <oleg.stone@gmail.com>
2022-04-24 21:50:25 +0300

    pom

--- src/main/java/ru/stn/telegram/govnoed/Application.java
+++ src/main/java/ru/stn/telegram/govnoed/Application.java
@@ -1,13 +1,14 @@
 package ru.stn.telegram.govnoed;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
+//@EnableJpaRepositories
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
