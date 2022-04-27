1a5010f2bb8a386f504dcd53bb644d9706802570
silexoider <oleg.stone@gmail.com>
2022-04-27 18:50:29 +0300

    handling

--- src/main/java/ru/stn/telegram/govnoed/handling/BaseHandler.java
+++ src/main/java/ru/stn/telegram/govnoed/handling/BaseHandler.java
@@ -3,16 +3,20 @@ package ru.stn.telegram.govnoed.handling;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-public abstract class BaseHandler<TKey, TRes, TSel, TArg> implements Handler<TKey, TRes, TSel, TArg> {
+public abstract class BaseHandler<TKey, TRes, TSel, TArg> {
     private final EntryContainer<TKey, BiFunction<TSel, TArg, TRes>, TSel> container = createContainer();
 
     protected BaseHandler(Iterable<Map.Entry<TKey, BiFunction<TSel, TArg, TRes>>> entries) {
+        init(entries);
+    }
+
+    protected void init(Iterable<Map.Entry<TKey, BiFunction<TSel, TArg, TRes>>> entries) {
         for (Map.Entry<TKey, BiFunction<TSel, TArg, TRes>> entry : entries) {
             container.insert(entry.getKey(), entry.getValue());
         }
     }
 
-    public TRes handle(TSel selector, TArg argument) {
+    protected TRes handle(TSel selector, TArg argument) {
         BiFunction<TSel, TArg, TRes> action = container.select(selector);
         if (action != null) {
             return action.apply(selector, argument);
--- src/main/java/ru/stn/telegram/govnoed/handling/Handler.java
+++
@@ -1,5 +0,0 @@
-package ru.stn.telegram.govnoed.handling;
-
-public interface Handler<TKey, TRes, TSel, TArg> {
-    TRes handle(TSel selector, TArg argument);
-}
